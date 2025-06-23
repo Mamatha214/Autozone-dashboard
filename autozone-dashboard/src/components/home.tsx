0d3753e946538ba52b0d02200141e57e8dc4a56e
mamatha <167530165+Mamatha214@users.noreply.github.com>
2025-06-23 21:46:21 +0530

    added navbar page

---
+++ autozone-dashboard/src/components/home.tsx
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+    
+
+  return (
+    <div className="home">
+      <h1>Welcome!</h1>
+     
+        <button>Logout</button>
+    
+    </div>
+  );
+}
+export default Home;
