974ad50ca4126d219c3845b4f0a0633fa280465e
mamatha <167530165+Mamatha214@users.noreply.github.com>
2025-06-23 23:47:41 +0530

    navbar edited

--- autozone-dashboard/tailwind.config.js
+++ autozone-dashboard/tailwind.config.js
@@ -1,8 +1,11 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // or wherever your files are
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
